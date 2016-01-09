e4f58b52d690df73051d0066451b1ea8aba30789
Christian Panadero <panavtec@gmail.com>
2016-01-09 17:51:06 +0100

    Initial commit with working files

---
+++ setup/setup.sh
@@ -0,0 +1,126 @@
+#!/usr/bin/env bash
+set -e
+
+ask() {
+  # http://djm.me/ask
+  while true; do
+
+    if [ "${2:-}" = "Y" ]; then
+      prompt="Y/n"
+      default=Y
+    elif [ "${2:-}" = "N" ]; then
+      prompt="y/N"
+      default=N
+    else
+      prompt="y/n"
+      default=
+    fi
+
+    # ask the question
+    read -p "$1 [$prompt] " REPLY
+
+    # default?
+    if [ -z "$REPLY" ]; then
+       REPLY=$default
+    fi
+
+    # check if the reply is valid
+    case "$REPLY" in
+      Y*|y*) return 0 ;;
+      N*|n*) return 1 ;;
+    esac
+
+  done
+}
+
+installi3() {
+  echo "Installing i3 and required tools"
+  sleep 2
+  yaourt --noconfirm -S \
+    i3 \
+    j4-dmenu-desktop \
+    i3blocks \
+    i3lock-blur \
+    xtitle \
+    xdotool \
+    feh \
+    unclutter \
+    scrot
+}
+
+installFonts() {
+  echo "Installing fonts"
+  sleep 2
+  yaourt --noconfirm -S \
+      ttf-font-awesome \
+      ttf-google-fonts-git \
+      ttf-ms-fonts
+}
+
+installThemes() {
+  echo "Installing themes"
+  sleep 2
+  yaourt --noconfirm -S \
+      paper-gtk-theme-git \
+      paper-icon-theme-git \
+      numix-icon-theme-git \
+      lxapparence
+}
+
+installDevTools() {
+  echo "Installing developer tools"
+  sleep 2
+  yaourt --noconfirm -S \
+      jdk \
+      jdk7 \
+      jd-gui-bin \
+      android-file-transfer \
+      android-studio \
+      diffmerge \
+      atom-editor \
+      genymotion \
+      gitflow-git \
+      smartgit
+}
+
+installTools() {
+  echo "Installing apps and tools"
+  sleep 2
+  yaourt --noconfirm -S \
+      firefox \
+      dropbox \
+      google-chrome \
+      google-talkplugin \
+      spotify \
+      archey3
+}
+
+dir=`pwd`
+if [ ! -e "${dir}/${0}" ]; then
+  echo "Script not called from within repository directory. Aborting."
+  exit 2
+fi
+dir="${dir}/.."
+
+echo "PaNaVTEC dotfiles installer"
+# Makes dir for scrot screenshots
+mkdir ${HOME}/Pictures/Screenshots
+
+#Makes binary executable
+chmod a+x ${dir}/bin/*
+
+ask "install i3?" Y && installi3
+ask "install fonts?" Y && installFonts
+ask "install dev tools?" Y && installDevTools
+ask "install apps and tools?" Y && installTools
+ask "install themes?" Y && installThemes
+
+ask "Install symlink for .xinitrc?" Y && ln -sfn ${dir}/.xinitrc ${HOME}/.xinitrc
+ask "Install symlink for .bashrc?" Y && ln -sfn ${dir}/.bashrc ${HOME}/.bashrc
+ask "Install symlink for .bash_profile?" Y && ln -sfn ${dir}/.bash_profile ${HOME}/.bash_profile
+
+ask "Install configuration for bin?" Y && ln -sfn ${dir}/bin ${HOME}/.bin
+ask "Install configuration for i3?" Y && ln -sfn ${dir}/config/i3 ${HOME}/.config/i3
+ask "Install configuration for i3blocks?" Y && ln -sfn ${dir}/.i3blocks.conf ${HOME}/.i3blocks.conf; ln -sfn ${dir}/config/i3blocks ${HOME}/.config/i3blocks
+ask "Install configuration for dunst?" Y && ln -sfn ${dir}/config/dunst ${HOME}/.config/dunst
+ask "Install configuration for termite?" Y && ln -sfn ${dir}/config/termite ${HOME}/.config/termite
