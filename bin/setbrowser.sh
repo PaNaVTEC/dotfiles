e3ef9c3489f44c971862f9beb1cf4ee424e57f78
Christian Panadero <panavtec@gmail.com>
2016-04-02 08:08:24 +0100

    Added script for setting default browser

---
+++ bin/setbrowser.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+NEWBROWSER=google-chrome-stable.desktop
+
+xdg-mime default $NEWBROWSER x-scheme-handler/http
+xdg-mime default $NEWBROWSER x-scheme-handler/https
+xdg-mime default $NEWBROWSER text/html
+xdg-settings set default-web-browser $NEWBROWSER
