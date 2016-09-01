cb1a19ea2eacb15acdd0ea1dc75469bdb659a1e5
panavtec <panavtec@gmail.com>
2016-09-01 15:56:52 +0100

    fixed chmod issue

--- laptops/MSI-GT60/setup.sh
+++ laptops/MSI-GT60/setup.sh
@@ -7,7 +7,8 @@ sudo cp 70-synaptics.conf /etc/X11/xorg.conf.d/
 #Sets nvidia as default GPU
 sudo pacman -S nvidia
 sudo cp xorg.conf /etc/X11/xorg.conf
-bash -c 'echo -e "xrandr --setprovideroutputsource modesetting NVIDIA-0 \nxrandr --auto" > /etc/X11/xinit/xinitrc.d/10-nvidia-load.sh'
+bash -c 'echo -e "xrandr --setprovideroutputsource modesetting NVIDIA-0 \nxrandr --auto" > /etc/X11/xinit/xinitrc.d/80-nvidia-load.sh'
+sudo chmod a+x /etc/X11/xinit/xinitrc.d/80-nvidia-load.sh
 
 #Fixes atheros 9285 connection drop problems
 bash -c 'echo -e "options ath9k nohwcrypt=1 ps_enable=0" > /etc/modprobe.d/ath9k.conf'
