eb4fd6f180557f7bdb3f902454a473295507e86a
Christian Panadero <panavtec@gmail.com>
2016-12-23 14:13:54 +0000

    fixed fn keys for macbook

--- laptops/macbookPro12,1/setup.sh
+++ laptops/macbookPro12,1/setup.sh
@@ -1,10 +1,12 @@
 yaourt -S --noconfirm 
 	xf86-input-mtrack-git \
-	kbdlight
+	kbdlight \
+	broadcom-wl
 
-#Fixes dpi of i3 in general
+#Fixes dpi of i3
 bash -c 'echo -e "xrandr --dpi 160\nxrdb -merge ~/.Xresources" >> ~/.before_startx/run.sh'
 chmod a+x ~/.before_startx/run.sh
-
 bash -c 'echo -e "Xft.dpi: 160.0" >> ~/.Xresources'
 
+#Fns instead of media keys
+sudo cp apple.conf /etc/modprobe.d/apple.conf
