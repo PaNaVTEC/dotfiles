28d5df0addbb21365f562920f57a7684afd08d52
Christian Panadero <panavtec@gmail.com>
2017-02-03 22:09:15 +0000

    add ruby to the path

--- paths.sh
+++ paths.sh
@@ -2,3 +2,4 @@ export PATH=${PATH}:~/Android/Sdk/tools/
 export PATH=${PATH}:~/Android/Sdk/platform-tools/
 export PATH=${PATH}:/opt/pypy3/bin/
 export PATH=${PATH}:~/.bin/
+export PATH=${PATH}:~/.gem/ruby/2.4.0/bin
