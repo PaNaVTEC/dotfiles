001127cd1c3c7f822c1e1581d435019ae7266905
Christian Panadero <panavtec@gmail.com>
2017-02-22 10:41:11 +0000

    add prune and discard with alias, moved custom config to its own file

---
+++ git-alias-custom.sh
@@ -0,0 +1,14 @@
+alias gpl='git prunelocal'
+alias gdl='git discard'
+
+## Autocompleted alias
+__git_complete gco _git_checkout
+__git_complete gb _git_branch
+__git_complete gba _git_branch
+__git_complete gbd _git_branch
+__git_complete gbda _git_branch
+__git_complete gbl _git_branch
+__git_complete gbnm _git_branch
+__git_complete gbr _git_branch
+__git_complete gm _git_branch
+__git_complete gd _git_branch
--- git-alias.sh
+++ git-alias.sh
@@ -243,14 +243,4 @@ alias glum='git pull upstream master'
 alias gwch='git whatchanged -p --abbrev-commit --pretty=medium'
 alias gwip='git add -A; git rm $(git ls-files --deleted) 2> /dev/null; git commit -m "--wip--"'
 
-## Autocompleted alias
-__git_complete gco _git_checkout
-__git_complete gb _git_branch
-__git_complete gba _git_branch
-__git_complete gbd _git_branch
-__git_complete gbda _git_branch
-__git_complete gbl _git_branch
-__git_complete gbnm _git_branch
-__git_complete gbr _git_branch
-__git_complete gm _git_branch
-__git_complete gd _git_branch
+source ~/dotfiles/git-alias-custom.sh
