d8bc29340ac766808035a369219f5f2c42ff2f05
Christian Panadero <panavtec@gmail.com>
2017-02-24 16:32:18 +0000

    if dropbox sync only output first column

--- bin/dropbox_status.sh
+++ bin/dropbox_status.sh
@@ -6,4 +6,8 @@
 #Dropbox is running: "Up to date..."
 STATUS="$(echo `dropbox-cli status`)"
 DROPBOX_ICON=""
-echo "$DROPBOX_ICON $STATUS"
+if [[ $STATUS = \Syncing* ]]; then
+  echo "$DROPBOX_ICON $(echo $STATUS | awk '{print $1}')"
+else
+  echo "$DROPBOX_ICON $STATUS"
+fi
