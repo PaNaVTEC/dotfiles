0483733f0eb15126c636f99e82915162f1ceed6e
Christian Panadero <panavtec@gmail.com>
2017-02-28 12:10:42 +0000

    add linter for clojure

--- config/lein/profiles.clj
+++ config/lein/profiles.clj
@@ -1,3 +1,4 @@
-{:user { :plugins [ [lein-ancient "0.6.10"] ]
+{:user { :plugins [ [lein-ancient "0.6.10"]
+                    [jonase/eastwood "0.2.3"]]
         :dependencies [[cljfmt "0.5.1"]] }
  :repl-options {:init (require 'cljfmt.core)} }
