08de1c81d9e01c51bfb2ada093995bedcb1a3c05
Christian Panadero <panavtec@gmail.com>
2017-07-11 17:04:17 +0100

    Launch polybar in every monitor

--- config/polybar/launch.sh
+++ config/polybar/launch.sh
@@ -6,4 +6,5 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
-polybar top &
+for i in $(polybar -m | awk -F: '{print $1}'); do POLYBAR_MONITOR=$i polybar top & done
+
