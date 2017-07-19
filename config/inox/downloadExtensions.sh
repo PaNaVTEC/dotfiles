0eff7f85dbbfe94196443e3fcc99c374d27be685
Christian Panadero <panavtec@gmail.com>
2017-07-19 18:58:03 +0100

    Fix extensions downlaod + add command

--- config/inox/downloadExtensions.sh
+++ config/inox/downloadExtensions.sh
@@ -1,8 +1,9 @@
 #!/bin/bash -e
 
 installationDirectory=~/Downloads
-for extensionId in $(ls ~/dotfiles/config/inox/installedExtensions | grep -v '^$'); do
-  (cd $installationDirectory && ./downloadExtensions.sh $extensionId)
+for extensionId in $(cat ~/dotfiles/config/inox/installedExtensions | grep -v '^$'); do
+  echo "Downloading $extensionId"
+  (cd $installationDirectory && downloadExtensionInox.sh $extensionId)
 done
 
 echo "Downloaded all extensions in $installationDirectory, open chrome: chrome://extensions/ and manually install"
