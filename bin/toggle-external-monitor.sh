989cdcce8a4af414b21f0d00d47eba8b473d455a
Christian Panadero <panavtec@gmail.com>
2017-08-23 09:54:01 +0100

    toggle external monitor

---
+++ bin/toggle-external-monitor.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+intern=LVDS-1
+extern=DP-1
+
+if xrandr | grep "$extern disconnected"; then
+    xrandr --output "$extern" --off
+    xrandr --output "$intern" --auto
+else
+    xrandr --output "$intern" --off
+    xrandr --output "$extern" --auto
+fi
