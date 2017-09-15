3c2b105f60d57ab9ab3412f3c4b58a25c50959c2
Christian Panadero <panavtec@gmail.com>
2017-09-15 13:47:26 +0100

    Created scripts for the js project function, introduced ES6 and standard

--- alias.sh
+++ alias.sh
@@ -25,7 +25,7 @@ alias dotfiles="(cd $dotfilesLocation && vim -c NERDTree)"
 alias ffs='sudo $(fc -ln -1)'
 
 ya () {
-  yarn $@ --emoji
+  yarn "$@" --emoji
 }
 
 javaProject () { 
@@ -35,35 +35,49 @@ javaProject () {
 }
 
 scalaProject () {
-  mkdir -p $1
-  sbt new scala/hello-world.g8 --name=$1
-  echo 'libraryDependencies += "org.scalactic" %% "scalactic" % "3.0.1"' >> $1/build.sbt
-  echo 'libraryDependencies += "org.scalatest" %% "scalatest" % "3.0.1" % "test"' >> $1/build.sbt
-  echo 'addSbtPlugin("org.scalastyle" %% "scalastyle-sbt-plugin" % "0.9.0")' >> $1/project/plugins.sbt
-  cd $1
+  projectDirectory="$1"
+  mkdir -p "$projectDirectory"
+  sbt new scala/hello-world.g8 --name="$projectDirectory"
+  echo 'libraryDependencies += "org.scalactic" %% "scalactic" % "3.0.1"' >> "$projectDirectory/build.sbt"
+  echo 'libraryDependencies += "org.scalatest" %% "scalatest" % "3.0.1" % "test"' >> "$projectDirectory/build.sbt"
+  echo 'addSbtPlugin("org.scalastyle" %% "scalastyle-sbt-plugin" % "0.9.0")' >> "$projectDirectory/project/plugins.sbt"
+  cd "$projectDirectory" || return
   sbt ensimeConfig
   sbt scalastyleGenerateConfig
 }
 
 alias jsProject='javascriptProject;'
 javascriptProject () {
-  mkdir -p $1
-  cd $1
-  yarn init -y
-  yarn add mocha chai standard
-  cp ~/dotfiles/config/vim/.tern-project .
-  mv package.json init.json
-  jq -r '.scripts |= . + {"start": "node src/index.js", "test": "mocha ./src/**.test.js"}' init.json > package.json
-  rm init.json
+  TAB="  "
+  mkdir -p "$1"
+  cd "$1" || return
+  ya init -y
+  ya add mocha chai standard
+  ya add babel-core babel-preset-es2015 --dev
+  cp "$dotfilesLocation/config/vim/tern-project" ./.tern-project
+  jq -r '.scripts |= . + {"start": "node src/index.js", "test": "mocha ./src/**.test.js --compilers js:babel-core/register"}' package.json > temp.json
+  jq -r '.standard |= . + {}' temp.json > package.json
+  jq -r '.standard.globals |= . + ["after", "afterEach", "before", "beforeEach", "describe", "context", "it"]' package.json > temp.json
+  mv temp.json package.json
   mkdir -p src
   touch src/index.js
-  yarn
+  echo -e "{\n$TAB\"presets\": [\"es2015\"]\n}" > .babelrc
+  local IMPORT="import { expect } from 'chai'"
+  local IT="${TAB}it('Tests something', () => {\n$TAB${TAB}expect(1).to.be.eql(1)\n$TAB})"
+  local DESCRIBE="describe('A test', () => {\n$IT\n})"
+  local TEST="$IMPORT\n\n$DESCRIBE"
+  echo -e "$TEST" > src/intext.test.js
+  ya
+}
+
+nb () {
+  "$(npm bin)/$1"
 }
 
 clojureProject() {
-  lein new $1
+  lein new "$1"
 }
 
 every() {
-  watch -c -n $1 $2
+  watch -c -n "$1" "$2"
 }
