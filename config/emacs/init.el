6df5463ffa9c9b94eed4b1008db8d8aaa42dc703
Christian Panadero <panavtec@gmail.com>
2017-09-16 21:38:31 +0100

    Add buffer navigation

--- config/emacs/init.el
+++ config/emacs/init.el
@@ -89,6 +89,12 @@
   (define-key evil-normal-state-map (kbd "gk") 'evil-previous-visual-line)
   (define-key evil-normal-state-map (kbd "g <up>") 'evil-previous-visual-line)
 
+  ;; Buffer navigation
+  (define-key evil-normal-state-map (kbd "C-w <up>") 'evil-window-up)
+  (define-key evil-normal-state-map (kbd "C-w <down>") 'evil-window-down)
+  (define-key evil-normal-state-map (kbd "C-w <left>") 'evil-window-left)
+  (define-key evil-normal-state-map (kbd "C-w <right>") 'evil-window-right)
+
   ;; Typo avoider
   (evil-ex-define-cmd "WQ" 'evil-save-and-quit)
   (evil-ex-define-cmd "Wq" 'evil-save-and-quit)
