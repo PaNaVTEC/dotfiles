f8de89f5ebc77ea21f010d4a6423113f7b649c5e
Christian Panadero <panavtec@gmail.com>
2017-09-16 11:00:31 +0100

    alias use-package to improve indenting of the file

--- config/emacs/init.el
+++ config/emacs/init.el
@@ -1,5 +1,4 @@
 ;; Bootstrap `use-package'
-
 (require 'package)
 (setq package-enable-at-startup nil)
 (add-to-list
@@ -13,6 +12,9 @@
   (package-refresh-contents)
   (package-install 'use-package))
 
+;; Functions
+(defalias 'pkg 'use-package)
+
 ;; Config
 (setq inhibit-startup-message t)
 (setq-default indent-tabs-mode nil)
@@ -25,28 +27,28 @@
 
 ;; Appearance
 
-(use-package
+(pkg
   powerline
   :ensure t
   :config
   (powerline-center-evil-theme)
 
-  (use-package flycheck-color-mode-line
-               :ensure t
-               :config
-               (add-hook 'flycheck-mode-hook 'flycheck-color-mode-line-mode)))
+  (pkg flycheck-color-mode-line
+       :ensure t
+       :config
+       (add-hook 'flycheck-mode-hook 'flycheck-color-mode-line-mode)))
 
-(use-package leuven-theme :ensure t)
+(pkg leuven-theme :ensure t)
 
 ;; Evil mode
 
-(use-package
+(pkg
   evil
   :ensure t
   :config
   (evil-mode 1)
 
-  (use-package
+  (pkg
     evil-leader
     :ensure t
     :config
@@ -55,50 +57,50 @@
     (evil-leader/set-key "e" 'find-file)
     )
 
-  (use-package
+  (pkg
     evil-surround
     :ensure t
     :config
     (global-evil-surround-mode))
 
-  (use-package evil-indent-textobject :ensure t)
+  (pkg evil-indent-textobject :ensure t)
 
   ;; Visual line navigation
   (define-key evil-normal-state-map (kbd "gj") 'evil-next-visual-line)
   (define-key evil-normal-state-map (kbd "gk") 'evil-previous-visual-line))
 
-(use-package company-mode :ensure t)
+(pkg company-mode :ensure t)
 
 ;; Haskell mode
-(use-package haskell-mode
-             :ensure t
-             :config
-             (use-package company-ghci :defer t))
+(pkg haskell-mode
+     :ensure t
+     :config
+     (pkg company-ghci :defer t))
 
-(use-package intero
-             :ensure t
-             :config
-             (add-hook 'haskell-mode-hook 'intero-mode))
+(pkg intero
+     :ensure t
+     :config
+     (add-hook 'haskell-mode-hook 'intero-mode))
 
-(use-package markdown-mode :ensure t)
-(use-package yaml-mode :ensure t)
+(pkg markdown-mode :ensure t)
+(pkg yaml-mode :ensure t)
 
 ;; Syntax check
-(use-package
+(pkg
   flycheck
   :ensure t
   :config
   (global-flycheck-mode))
 
 ;; Zeal setup
-(use-package zeal-at-point :ensure t)
+(pkg zeal-at-point :ensure t)
 
 (add-to-list 'zeal-at-point-mode-alist '(haskell-mode . "haskell"))
 (global-set-key "\C-cd" 'zeal-at-point)
 (add-to-list 'exec-path "/usr/bin/zeal")
 
 ;; Neotree
-(use-package
+(pkg
   neotree
   :ensure t
   :init
@@ -110,40 +112,40 @@
   (define-key evil-normal-state-map (kbd "C-n") 'neotree-toggle))
 
 ;; Rest client
-(use-package restclient :ensure t)
+(pkg restclient :ensure t)
 
 ;; Emacs line number
 (global-linum-mode t)
 
-(use-package smooth-scrolling
-             :ensure t
-             :init
-             (setq scroll-margin 5
-                   scroll-conservatively 9999
-                   scroll-step 1))
+(pkg smooth-scrolling
+     :ensure t
+     :init
+     (setq scroll-margin 5
+           scroll-conservatively 9999
+           scroll-step 1))
 
 ;; helm settings (TAB in helm window for actions over selected items,
 ;; C-SPC to select items)
-(use-package helm 
-             :ensure t
-             :init (use-package helm-projectile :ensure t)
-             :config
-             (require 'helm-config)
-             (require 'helm-misc)
-             (require 'helm-projectile)
-             (require 'helm-locate)
-             (helm-mode 1)
-             (define-key evil-normal-state-map " " 'helm-mini)
-             (setq helm-quick-update t)
-             (setq helm-bookmark-show-location t)
-             (setq helm-buffers-fuzzy-matching t)
+(pkg helm 
+     :ensure t
+     :init (pkg helm-projectile :ensure t)
+     :config
+     (require 'helm-config)
+     (require 'helm-misc)
+     (require 'helm-projectile)
+     (require 'helm-locate)
+     (helm-mode 1)
+     (define-key evil-normal-state-map " " 'helm-mini)
+     (setq helm-quick-update t)
+     (setq helm-bookmark-show-location t)
+     (setq helm-buffers-fuzzy-matching t)
 
-             ;; Ctrlp like
-             (global-set-key (kbd "C-p") 'projectile--find-file)
-             (define-key evil-normal-state-map (kbd "C-p") 'projectile--find-file)
+     ;; Ctrlp like
+     (global-set-key (kbd "C-p") 'projectile--find-file)
+     (define-key evil-normal-state-map (kbd "C-p") 'projectile--find-file)
 
-             ;; Override default command launcher
-             (global-set-key (kbd "M-x") 'helm-M-x))
+     ;; Override default command launcher
+     (global-set-key (kbd "M-x") 'helm-M-x))
 
 ;; Emacs global
 (global-set-key (kbd "C-l") 'evil-search-highlight-persist-remove-all)
