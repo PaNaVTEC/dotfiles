b6d26057f6772dae88d1b9606ad827e1e53448d4
Christian Panadero <panavtec@gmail.com>
2017-09-17 14:48:53 +0100

    Bindings for JSRefactor + prettify

--- config/emacs/lisp/javascript.el
+++ config/emacs/lisp/javascript.el
@@ -8,14 +8,36 @@
         evil-shift-width 2
         js2-basic-offset 2
         js2-strict-missing-semi-warning nil)
-  ;(setq js2-mode-show-strict-warnings nil)
-  ;(setq js2-mode-show-parse-errors nil)
+
+  (defun javascript/prettify ()
+    (prettify
+      '(("<-" . ?←)
+        ("function" . ?ƒ)
+        ("NaN" . ?ℕ)
+        ("this" . ?@)
+        ("=>" . ?⇒)
+        ("<=" . ?≤)
+        (">=" . ?≥)
+        ("!=" . ?≠))))
+  (add-hook 'js2-mode-hook 'prettify-symbols-mode)
+  (add-hook 'js2-mode-hook 'javascript/prettify)
+
+  ;; Lint
   (add-hook 'flycheck-mode-hook
             (lambda () (flycheck-add-mode 'javascript-standard 'js2-mode))))
 
 (pkg rjsx-mode :ensure t :mode "\\.jsx$" :mode "components/.+\\.js$")
 (pkg tern :defer t :init (add-hook 'js2-mode-hook 'tern-mode))
-(pkg js2-refactor :ensure t)
+(pkg
+  js2-refactor
+  :ensure t
+  :config
+  (evil-leader/set-key "v" 'js2r-extract-var)
+  (evil-leader/set-key "m" 'js2r-extract-method)
+  (evil-leader/set-key "f" 'js2r-extract-function)
+  (evil-leader/set-key "n" 'js2r-inline-var)
+  (evil-leader/set-key "r" 'js2r-rename-var))
+
 (pkg json-mode :ensure t :mode "\\.json\\'")
 (pkg
   company-tern
