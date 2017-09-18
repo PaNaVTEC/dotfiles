f84baa645559832d50c92c18056e3eb54850b791
Christian Panadero <panavtec@gmail.com>
2017-09-18 17:47:32 +0100

    Add git gutter

--- config/emacs/lisp/relative-lines.el
+++ config/emacs/lisp/relative-lines.el
@@ -6,6 +6,15 @@
   (linum-relative-global-mode)
   (setq
     linum-relative-current-symbol ""
-    linum-relative-format "%3s ")) ;; Add \u2502 for more separation
+    linum-relative-format "%3s ") ;; Add \u2502 for more separation
+
+  (pkg
+    git-gutter
+    :ensure t
+    :diminish 'git-gutter-mode
+    :defer 0.5 ;; Fix for relative-line-number
+    :config
+    (global-git-gutter-mode t)
+    (git-gutter:linum-setup)))
 
 (provide 'relative-lines)
