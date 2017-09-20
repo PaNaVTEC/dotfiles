e6515aae3b79d96054e5f189e90b680045c30767
Christian Panadero <panavtec@gmail.com>
2017-09-20 21:39:00 +0100

    Fixes jsx indentation

--- config/emacs/lisp/javascript.el
+++ config/emacs/lisp/javascript.el
@@ -9,6 +9,7 @@
   (setq js-indent-level 2
         evil-shift-width 2
         js2-basic-offset 2
+        js2-bounce-indent-p t
         js2-strict-missing-semi-warning nil)
 
   (defun javascript/prettify ()
@@ -28,7 +29,13 @@
   (add-hook 'flycheck-mode-hook
             (lambda () (flycheck-add-mode 'javascript-standard 'js2-mode))))
 
-(pkg rjsx-mode :ensure t :mode "\\.jsx$" :mode "components/.+\\.js$")
+(pkg web-mode
+     :ensure t
+     :mode "\\.jsx$"
+     :mode "components/.+\\.js$"
+     :config
+     (setq web-mode-code-indent-offset 2))
+
 (pkg tern :defer t :init (add-hook 'js2-mode-hook 'tern-mode))
 (pkg
   js2-refactor
