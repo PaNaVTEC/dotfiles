124ff4343bec62501fa474fcd266d86c0ca93206
Christian Panadero <panavtec@gmail.com>
2017-09-20 22:17:13 +0100

    Remove custom given that it auto recreates this

--- config/emacs/init.el
+++ config/emacs/init.el
@@ -32,5 +32,24 @@
   (require module))
 
 (provide 'init)
-(provide 'custom)
 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(git-gutter:added-sign "+")
+ '(git-gutter:deleted-sign "-")
+ '(git-gutter:modified-sign "~")
+ '(git-gutter:verbosity 0)
+ '(git-gutter:visual-line t)
+ '(git-gutter:window-width 1)
+ '(package-selected-packages
+   (quote
+    (web-mode zenburn-theme zeal-at-point yaml-mode use-package telephone-line spacemacs-theme spaceline smooth-scrolling rjsx-mode restclient nyan-mode neotree monokai-theme moe-theme material-theme markdown-mode linum-relative json-mode js2-refactor intero helm-projectile hc-zenburn-theme git-gutter flycheck-color-mode-line flatland-theme evil-surround evil-numbers evil-leader evil-indent-textobject ensime darkokai-theme company-tern))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
