1de34e343fd57a171e0c09c7eed4aaefe7b66d59
Christian Panadero <panavtec@gmail.com>
2017-09-22 10:27:16 +0100

    Fixed mode to display eol chars in grey over grey

--- config/emacs/lisp/invisible-chars.el
+++ config/emacs/lisp/invisible-chars.el
@@ -1,12 +1,12 @@
-(use-package
+(pkg
   whitespace
+  :diminish whitespace-mode
   :init
   (progn
     (setq whitespace-line-column 80)
-    (setq whitespace-style '(face tabs empty trailing lines-tail newline-mark))
-    (setq
-      whitespace-display-mappings
-      '((newline-mark 10 [172 10])))))
-  :config
-    (evil-leader/set-key "w" 'whitespace-cleanup)
+    (setq whitespace-style '(face trailing tabs newline tab-mark lines-tail newline-mark))
+    (setq whitespace-display-mappings '((newline-mark 10 [172 10]))))
+:config
+  (evil-leader/set-key "w" 'whitespace-cleanup))
+
 (provide 'invisible-chars)
