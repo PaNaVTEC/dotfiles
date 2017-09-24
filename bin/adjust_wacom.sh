f80683c07175881410a6732c0527968f4adf543f
Christian Panadero <panavtec@gmail.com>
2017-09-24 21:37:54 +0100

    remove unused scripts

--- bin/adjust_wacom.sh
+++
@@ -1,4 +0,0 @@
-xinput set-prop "Wacom Intuos PT S 2 Finger" "Device Accel Constant Deceleration" 0.3
-xinput set-prop "Wacom Intuos PT S 2 Finger" "Synaptics Two-Finger Scrolling" 1 1
-xinput set-prop "Wacom Intuos PT S 2 Finger" "Synaptics Scrolling Distance" 25 25
-                                                                                                                                                                            
--- bin/wacomconfigure.sh
+++
@@ -1,5 +0,0 @@
-xinput list-props "Wacom Intuos PT S 2 Finger"
-xinput set-prop "Wacom Intuos PT S 2 Finger" "Synaptics Two-Finger Scrolling" 1, 1
-xinput set-prop "Wacom Intuos PT S 2 Finger" "Device Accel Profile" 2
-xinput set-prop "Wacom Intuos PT S 2 Finger" "Device Accel Constant Deceleration" 1.6
-xinput set-prop "Wacom Intuos PT S 2 Finger" "Synaptics Scrolling Distance" 25 25
