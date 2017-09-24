4796fd0f54b7ebd3aada31f60255103d1c3e8625
Christian Panadero <panavtec@gmail.com>
2017-09-24 12:30:49 +0100

    Show flycheck buffer as location list

--- config/emacs/lisp/syntax-check.el
+++ config/emacs/lisp/syntax-check.el
@@ -3,6 +3,11 @@
   :ensure t
   :config
   (define-key evil-normal-state-map (kbd "]w") 'flycheck-next-error)
-  (define-key evil-normal-state-map (kbd "[w") 'flycheck-previous-error))
+  (define-key evil-normal-state-map (kbd "[w") 'flycheck-previous-error)
+  (add-to-list 'display-buffer-alist `(,(rx bos "*Flycheck errors*" eos)
+                                       (display-buffer-reuse-window display-buffer-in-side-window)
+                                       (side            . bottom)
+                                       (reusable-frames . visible)
+                                       (window-height   . 0.33))))
 
 (provide 'syntax-check)
