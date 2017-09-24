b8776fb546aec47546f7d8e6f8c8b15231c75ce3
Christian Panadero <panavtec@gmail.com>
2017-09-24 19:07:38 +0100

    remove useless function

--- config/emacs/lisp/rainbow-parenthesis.el
+++ config/emacs/lisp/rainbow-parenthesis.el
@@ -1,13 +1,3 @@
-(pkg
-  rainbow-delimiters
-  :ensure t
-  :config
-  (require 'cl-lib)
-  (require 'color)
-  (cl-loop
-    for index from 1 to rainbow-delimiters-max-face-count
-    do
-    (let ((face (intern (format "rainbow-delimiters-depth-%d-face" index))))
-      (cl-callf color-saturate-name (face-foreground face) 30))))
+(pkg rainbow-delimiters :ensure t)
 
 (provide 'rainbow-parenthesis)
