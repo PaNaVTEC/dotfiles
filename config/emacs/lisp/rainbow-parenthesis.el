ae471b15f8a6f938a5ab03c9f351e7bd28138b92
Christian Panadero <panavtec@gmail.com>
2017-09-24 08:55:12 +0100

    Get rainbow delimiters color less subtle

--- config/emacs/lisp/rainbow-parenthesis.el
+++ config/emacs/lisp/rainbow-parenthesis.el
@@ -1,5 +1,13 @@
 (pkg
   rainbow-delimiters
-  :ensure t)
+  :ensure t
+  :config
+  (require 'cl-lib)
+  (require 'color)
+  (cl-loop
+    for index from 1 to rainbow-delimiters-max-face-count
+    do
+    (let ((face (intern (format "rainbow-delimiters-depth-%d-face" index))))
+      (cl-callf color-saturate-name (face-foreground face) 30))))
 
 (provide 'rainbow-parenthesis)
