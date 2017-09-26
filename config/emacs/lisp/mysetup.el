198371fa70371e8222ced5b66972bc4c3af1be62
Christian Panadero <panavtec@gmail.com>
2017-09-26 09:40:10 +0100

    Hack to nord theme to show comments

--- config/emacs/lisp/mysetup.el
+++ config/emacs/lisp/mysetup.el
@@ -6,6 +6,11 @@
 (defcustom available-themes '(nord spacemacs-light) "Available themes to cycle")
 (pkg zenburn-theme :ensure t)
 (pkg spacemacs-theme :ensure t)
-(pkg nord-theme :ensure t)
+(pkg nord-theme :ensure t :config
+     (set-face-attribute 'font-lock-comment-face nil
+                         :foreground "#81A1C1") ; nord9
+     (set-face-attribute 'vertical-border nil
+                         :foreground "#EBCB8B") ; nord13
+     )
 
 (provide 'mysetup)
