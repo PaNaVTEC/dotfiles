fec61b349ce311bea171f8e50904081ba498e6ab
Christian Panadero <panavtec@gmail.com>
2017-09-27 08:20:50 +0100

    Fixes background color while using nord

--- config/emacs/lisp/mysetup.el
+++ config/emacs/lisp/mysetup.el
@@ -4,13 +4,11 @@
 
 ;; Themes
 (defcustom available-themes '(nord spacemacs-light) "Available themes to cycle")
-(pkg zenburn-theme :ensure t)
 (pkg spacemacs-theme :ensure t)
 (pkg nord-theme :ensure t :config
-     (set-face-attribute 'font-lock-comment-face nil
-                         :foreground "#81A1C1") ; nord9
-     (set-face-attribute 'vertical-border nil
-                         :foreground "#EBCB8B") ; nord13
-     )
+  ;; nord9
+  (set-face-attribute 'font-lock-comment-face nil :foreground "#81A1C1")
+  ;; nord13
+  (set-face-attribute 'vertical-border nil :foreground "#EBCB8B"))
 
 (provide 'mysetup)
