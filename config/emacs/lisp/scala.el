e9222db821362bb00d0e1ee4313183a8b67434e1
Christian Panadero <panavtec@gmail.com>
2017-09-27 19:45:53 +0100

    Add some key bindings, disable flycheck scala as it compiles twice

--- config/emacs/lisp/scala.el
+++ config/emacs/lisp/scala.el
@@ -7,17 +7,17 @@
   :config
   (defun scala/prettify ()
     (prettify
-      '(("->" . ?→)
-        ("def" . ?ƒ)
-        ("<-" . ?←)
-        ("=>" . ?⇒)
-        ("==>" . ?⟹)
-        ("<=" . ?≤)
-        (">=" . ?≥)
-        ("::" . ?∷)
-        ("==" . ?≡)
-        ("!=" . ?≠)
-        ("???" . ?⊥))))
+     '(("->" . ?→)
+       ("def" . ?ƒ)
+       ("<-" . ?←)
+       ("=>" . ?⇒)
+       ("==>" . ?⟹)
+       ("<=" . ?≤)
+       (">=" . ?≥)
+       ("::" . ?∷)
+       ("==" . ?≡)
+       ("!=" . ?≠)
+       ("???" . ?⊥))))
   (add-hook 'scala-mode-hook 'programming-mode)
   (add-hook 'scala-mode-hook 'scala/prettify))
 
@@ -27,9 +27,27 @@
   :pin melpa-stable
   :config
   (setq
-    ensime-startup-notification nil
-    ensime-startup-snapshot-notification nil
-    ensime-use-helm t)
+   ensime-startup-notification nil
+   ensime-startup-snapshot-notification nil
+   ensime-use-helm t)
+  (define-key evil-normal-state-map (kbd "]w") 'ensime-forward-note)
+  (define-key evil-normal-state-map (kbd "[w") 'ensime-backward-note)
+
+  ;; Info
+  (evil-leader/set-key "p" 'ensime-type-at-point-full-name)
+  (evil-leader/set-key "q" 'ensime-show-doc-for-symbol-at-point)
+  (evil-leader/set-key "h" 'ensime-show-hierarchy-of-type-at-point)
+
+  ;; Refactor
+  (evil-leader/set-key "O" 'ensime-refactor-diff-organize-imports)
+  (evil-leader/set-key "l" 'ensime-refactor-diff-extract-local)
+  (evil-leader/set-key "m" 'ensime-refactor-diff-extract-method)
+
+  ;; Run
+  (evil-leader/set-key "r" 'ensime-db-run)
+
+  (add-to-list 'flycheck-disabled-checkers 'scala)
+
   (add-hook 'scala-mode-hook 'ensime))
 (pkg sbt-mode :ensure t)
 
