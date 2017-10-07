da2b7b8215405b18276967e93b111e6c2bb8eb05
Christian Panadero <panavtec@gmail.com>
2017-10-07 21:33:56 +0100

    Add haskell project creationg with hspec

--- programming/haskell.sh
+++ programming/haskell.sh
@@ -5,5 +5,17 @@ alias ghci='stack exec ghci --'
 
 createHaskellProject () {
   local projectDirectory=$1
-  stack new "$projectDirectory"
+  local name=''
+  local email=''
+  local username=''
+  name=$(cat ~/.gitconfig | grep name | awk -F '=' '{print $2}')
+  email=$(cat ~/.gitconfig | grep email | awk -F '=' '{print $2}')
+  username=$(echo "$email" | awk -F '@' '{print $1}')
+
+  stack new "$projectDirectory" hspec \
+    -p "author-email:$email"\
+    -p "author-name:$name" \
+    -p "category:katas" \
+    -p "copyright:Apache2" \
+    -p "github-username:$username"
 }
