e7e0eedcb866daa4bdf68f820df367cad26c7587
Christian Panadero <panavtec@gmail.com>
2017-10-07 16:15:20 +0100

    Add flycheck error messags to the location list buffer

--- config/emacs/lisp/syntax-check.el
+++ config/emacs/lisp/syntax-check.el
@@ -4,6 +4,7 @@
   :config
   (define-key evil-normal-state-map (kbd "]w") 'flycheck-next-error)
   (define-key evil-normal-state-map (kbd "[w") 'flycheck-previous-error)
-  (location-list-buffer (rx bos "*Flycheck errors*")))
+  (location-list-buffer (rx bos "*Flycheck errors*"))
+  (location-list-buffer (rx bos "*Flycheck error messages*")))
 
 (provide 'syntax-check)
