8e69660847401f50e279b3869534cb0bbfbcb19d
Christian Panadero <panavtec@gmail.com>
2017-10-20 16:13:12 +0100

    Fix invisible chars visualization

--- config/emacs/lisp/invisible-chars.el
+++ config/emacs/lisp/invisible-chars.el
@@ -1,12 +1,25 @@
 (pkg
   whitespace
+  :ensure t
   :diminish whitespace-mode
-  :init
+  :config
   (progn
     (setq whitespace-line-column 80)
-    (setq whitespace-style '(face trailing leading tabs newline tab-mark lines-tail newline-mark))
-    (setq whitespace-display-mappings '((newline-mark 10 [172 10]))))
-:config
-  (evil-leader/set-key "w" 'whitespace-cleanup))
+    (setq whitespace-style '(face
+                             indentation
+                             newline
+                             newline-mark
+                             empty
+                             lines-tail
+                             space-after-tab::tab
+                             space-before-tab::tab
+                             space-mark
+                             tab-mark
+                             tabs
+                             trailing))
+    (setq whitespace-display-mappings '((newline-mark 10 [172 10])
+                                        (space-mark 32 [183] [46])))
+    (set-face-attribute 'whitespace-trailing nil :background "magenta")
+    (evil-leader/set-key "w" 'whitespace-cleanup)))
 
 (provide 'invisible-chars)
