be0f6bc1177d19113d8e5a5a871f02ff5809bdfc
Christian Panadero <panavtec@gmail.com>
2017-10-20 16:22:28 +0100

    Add empty lines to highlight

--- config/emacs/lisp/invisible-chars.el
+++ config/emacs/lisp/invisible-chars.el
@@ -13,13 +13,13 @@
                              lines-tail
                              space-after-tab::tab
                              space-before-tab::tab
-                             space-mark
                              tab-mark
                              tabs
                              trailing))
     (setq whitespace-display-mappings '((newline-mark 10 [172 10])
                                         (space-mark 32 [183] [46])))
     (set-face-attribute 'whitespace-trailing nil :background "magenta")
+    (set-face-attribute 'whitespace-empty nil :background "magenta")
     (evil-leader/set-key "w" 'whitespace-cleanup)))
 
 (provide 'invisible-chars)
