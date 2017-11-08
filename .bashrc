8080ad35b25214ac843dbd35dc91c650a1c63e44
Christian Panadero <panavtec@gmail.com>
2017-11-08 18:16:38 +0000

    Update yarn-completion, add currency convert

--- .bashrc
+++ .bashrc
@@ -6,6 +6,7 @@ export SSH_AUTH_SOCK="$XDG_RUNTIME_DIR/ssh-agent.socket"
 export DOTFILES_LOCATION=~/dotfiles
 
 source "$DOTFILES_LOCATION/programming/index.sh"
+source "$DOTFILES_LOCATION/finance/index.sh"
 source "$DOTFILES_LOCATION/system/index.sh"
 source "$DOTFILES_LOCATION/paths.sh"
 source ~/.env.sh
---
+++ finance/index.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -e
+
+currencyConvert() {
+  curl -s "https://finance.google.com/finance/converter?a=$1&from=$2&to=$3" | sed '/res/!d;s/<[^>]*>//g'
+}
+
+currencyConvertPorcelain() {
+  currencyConvert "$1" "$2" "$3" | awk -F'=' '{print $2}'
+}
+
+gbpToEur() {
+  currencyConvert 1 'GBP' 'EUR'
+}
+
+gbpToUsd() {
+  currencyConvert 1 'GBP' 'USD'
+}
