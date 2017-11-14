bc341a4342e00bbf2487bc636e5a362d33da5081
Christian Panadero <panavtec@gmail.com>
2017-11-14 17:44:20 +0000

    Bye bye <C-g> now ESC is for everything

--- config/emacs/lisp/vim-mode.el
+++ config/emacs/lisp/vim-mode.el
@@ -16,12 +16,34 @@
   (define-key evil-motion-state-map (kbd "C-w <down>") 'evil-window-down)
   (define-key evil-motion-state-map (kbd "C-w <left>") 'evil-window-left)
   (define-key evil-motion-state-map (kbd "C-w <right>") 'evil-window-right)
+  (define-key evil-motion-state-map (kbd "C-w k") 'evil-window-up)
+  (define-key evil-motion-state-map (kbd "C-w j") 'evil-window-down)
+  (define-key evil-motion-state-map (kbd "C-w h") 'evil-window-left)
+  (define-key evil-motion-state-map (kbd "C-w l") 'evil-window-right)
 
   ;; Buffer navigation
   (define-key evil-motion-state-map (kbd "[ b") 'evil-prev-buffer)
   (define-key evil-motion-state-map (kbd "] b") 'evil-next-buffer)
 
   (global-set-key (kbd "C-l") 'evil-search-highlight-persist-remove-all)
+
+  ;;; esc quits
+  (defun minibuffer-keyboard-quit ()
+    "Abort recursive edit.
+  In Delete Selection mode, if the mark is active, just deactivate it;
+  then it takes a second \\[keyboard-quit] to abort the minibuffer."
+    (interactive)
+    (if (and delete-selection-mode transient-mark-mode mark-active)
+        (setq deactivate-mark  t)
+      (when (get-buffer "*Completions*") (delete-windows-on "*Completions*"))
+      (abort-recursive-edit)))
+  (define-key evil-normal-state-map [escape] 'keyboard-quit)
+  (define-key evil-visual-state-map [escape] 'keyboard-quit)
+  (define-key minibuffer-local-map [escape] 'minibuffer-keyboard-quit)
+  (define-key minibuffer-local-ns-map [escape] 'minibuffer-keyboard-quit)
+  (define-key minibuffer-local-completion-map [escape] 'minibuffer-keyboard-quit)
+  (define-key minibuffer-local-must-match-map [escape] 'minibuffer-keyboard-quit)
+  (define-key minibuffer-local-isearch-map [escape] 'minibuffer-keyboard-quit)
 
   ;; Reload config
   (evil-ex-define-cmd "ReloadConfig" '(lambda () (interactive) (load-file "~/.emacs.d/init.el")))
