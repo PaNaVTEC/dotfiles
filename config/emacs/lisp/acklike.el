687d7977e480be22c8c8c4aa3083f4a253f964d2
Christian Panadero <panavtec@gmail.com>
2017-11-14 21:21:40 +0000

    Ignore directories while searching

--- config/emacs/lisp/acklike.el
+++ config/emacs/lisp/acklike.el
@@ -5,5 +5,8 @@
          (rgrep arg "*.*" "./")))
 
 (evil-ex-define-cmd "Ack" 'evil-ack)
+(customize-set-variable
+  'grep-find-ignored-directories
+  (list "SCCS" "RCS" "CVS" "MCVS" ".svn" ".git" ".hg" ".bzr" "_MTN" "_darcs" "{arch}" "objects" "build" "bin" "out" "lib" "dist" "node_modules"))
 
 (provide 'acklike)
