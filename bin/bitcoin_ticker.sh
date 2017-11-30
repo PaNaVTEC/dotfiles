d5029ca610414aba2989e9c784ac41db6b4dd374
Christian Panadero <panavtec@gmail.com>
2017-11-30 09:33:58 +0000

    Add gdax btcticker

---
+++ bin/bitcoin_ticker.sh
@@ -0,0 +1,13 @@
+gdaxTicker () {
+  local res
+  res=$(curl -s "https://api.gdax.com/products/BTC-$1/ticker")
+  jq -r .ask <<< "$res"
+}
+
+krakenTicker () {
+  local res
+  res=$(curl -s "https://api.kraken.com/0/public/Ticker?pair=XXBTZ$1")
+  jq -r ".result.XXBTZ$1.c[0]" <<< "$res"
+}
+
+printf "%.2f € | $ %.2f" "$(gdaxTicker EUR)" "$(gdaxTicker USD)"
