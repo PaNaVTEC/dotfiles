f8b225efae0f9d4b87f57c0073cd728d25f941e3
Christian Panadero <panavtec@gmail.com>
2018-01-20 09:56:00 +0000

    Tweak welcome screen

--- config/emacs/lisp/init-welcome.el
+++ config/emacs/lisp/init-welcome.el
@@ -1,15 +1,19 @@
 ;; Initial mode in text avoids lag
-;(setq initial-major-mode 'text-mode
-;      initial-scratch-message nil)
+(setq ;initial-major-mode 'text-mode
+  initial-scratch-message nil)
+
+(pkg
+  page-break-lines
+  :ensure t)
 
 (pkg
   dashboard
   :ensure t
+  :after page-break-lines
   :config
-  (setq dashboard-startup-banner 'official)
-  (setq dashboard-items '((recents  . 5)
-                          (projects . 5)
-                          (registers . 5)))
-  (dashboard-setup-startup-hook))
+  (dashboard-setup-startup-hook)
+  (setq dashboard-startup-banner 'official
+        dashboard-items '((projects  . 10)
+                          (recents . 10))))
 
 (provide 'init-welcome)
