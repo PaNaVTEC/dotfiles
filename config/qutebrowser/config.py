0a6bbbee8924927f081ea7d10341f26efc45e316
Christian Panadero <panavtec@gmail.com>
2018-03-09 14:32:27 +0000

    Add haskell search engines

--- config/qutebrowser/config.py
+++ config/qutebrowser/config.py
@@ -1,7 +1,9 @@
 c.url.searchengines = {
-        'DEFAULT' : 'https://duckduckgo.com/?q={}',
-        'aw' : 'https://wiki.archlinux.org/?search={}',
-        'yt' : 'https://www.youtube.com/results?search_query={}',
-        'gh' : 'https://github.com/search?utf8=%E2%9C%93&q={}&type=Code',
-        'wi' : 'http://en.wikipedia.org/w/index.php?title=Special:Search&search={}'
-        }
+    'DEFAULT' : 'https://duckduckgo.com/?q={}',
+    'aw' : 'https://wiki.archlinux.org/?search={}',
+    'yt' : 'https://www.youtube.com/results?search_query={}',
+    'gh' : 'https://github.com/search?utf8=%E2%9C%93&q={}&type=Code',
+    'wi' : 'http://en.wikipedia.org/w/index.php?title=Special:Search&search={}',
+    'ha' : 'https://hackage.haskell.org/packages/search?terms={}',
+    'ho' : 'https://hoogle.haskell.org/?hoogle={}&scope=set%3Astackage'
+}
