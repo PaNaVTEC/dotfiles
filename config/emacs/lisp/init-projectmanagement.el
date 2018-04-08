10feaa57d3434bf1ec275a59bace6486b3bfd8c7
Christian Panadero <panavtec@gmail.com>
2018-04-08 08:32:26 +0100

    Fixes minibuffer in the latest version of projectile

--- config/emacs/lisp/init-projectmanagement.el
+++ config/emacs/lisp/init-projectmanagement.el
@@ -10,6 +10,6 @@
   :config
   (global-set-key (kbd "C-p") 'helm-projectile-find-file)
   (define-key evil-normal-state-map (kbd "C-p") 'helm-projectile-find-file)
-  (location-list-buffer (rx bos "*helm projectile*")))
+  (location-list-buffer (rx bos "*helm projectile")))
 
 (provide 'init-projectmanagement)
