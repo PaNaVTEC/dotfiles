80ecbe7cb8305db527ee25f0c681be59fea94f2d
Christian Panadero <panavtec@gmail.com>
2018-05-23 21:01:06 +0100

    add emacs config apps

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -1,7 +1,3 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
 { config, pkgs, ... }:
 
 {
@@ -11,6 +7,8 @@
       ./i3.nix
       ./urxvt.nix
       ./fonts.nix
+      ./apps.nix
+      ./emacs.nix
     ];
 
   # SSD options
