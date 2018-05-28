47c422e7d695cf115c7f37c435318182b49a52c2
Christian Panadero <panavtec@gmail.com>
2018-05-28 14:07:48 +0100

    patch terminus with td1

--- nixos/fonts.nix
+++ nixos/fonts.nix
@@ -1,9 +1,17 @@
 { config, pkgs, ... }:
 
+let
+  terminus-td1 = pkgs.stdenv.lib.overrideDerivation pkgs.terminus_font (oldAttrs : {
+    configurePhase = ''
+      patch < alt/td1.diff
+      sh ./configure --prefix=$out
+    '';
+  });
+in
 {
   fonts = {
     fonts = with pkgs; [
-      terminus_font
+      terminus-td1
       twemoji-color-font
       google-fonts
       noto-fonts-emoji
