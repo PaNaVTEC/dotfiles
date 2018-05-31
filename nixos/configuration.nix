61e53cf8dc0fbe9658a897492276348731d0a434
Carlos Morera de la Chica <carlosdelachica@gmail.com>
2018-05-31 19:40:50 +0100

    Add bash configuration to nixos

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -22,6 +22,7 @@ in
       ./git.nix
       ./virtualization.nix
       ./containerization.nix
+      ./bash.nix
     ];
 
   # SSD options
@@ -68,7 +69,6 @@ in
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
-  programs.bash.enableCompletion = true;
   # programs.mtr.enable = true;
   # programs.gnupg.agent = { enable = true; enableSSHSupport = true; };
 
