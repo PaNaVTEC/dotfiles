54dfdf526fadbb1952781ac169eede063c51ba27
Christian Panadero <panavtec@gmail.com>
2018-06-07 09:24:57 +0100

    use /home/panavtec instead of ~

--- paths.sh
+++ paths.sh
@@ -1,11 +1,10 @@
-export PATH=${PATH}:~/Android/Sdk/tools/
-export PATH=${PATH}:~/Android/Sdk/platform-tools/
-export PATH=${PATH}:/opt/pypy3/bin/
-export PATH=${PATH}:~/.bin/
-export PATH=${PATH}:~/.local/bin/
-export PATH=${PATH}:~/.gem/ruby/2.4.0/bin
-export PATH=${PATH}:~/.yarn/bin
+export PATH=$PATH:"$HOME/Android/Sdk/tools/"
+export PATH=$PATH:"$HOME/Android/Sdk/platform-tools/"
+export PATH=$PATH:"/opt/pypy3/bin/"
+export PATH=$PATH:"$HOME/.bin/"
+export PATH=$PATH:"$HOME/.local/bin/"
+export PATH=$PATH:"$HOME/.gem/ruby/2.4.0/bin"
+export PATH=$PATH:"$HOME/.yarn/bin"
 
-export GOPATH=~/go
+export GOPATH="$HOME/go"
 export PATH="${PATH}:$GOPATH/bin"
-
