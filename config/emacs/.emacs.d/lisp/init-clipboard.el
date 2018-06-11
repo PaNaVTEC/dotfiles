e668856deb4ee4030e0bd5ce92a23ca42b874434
Christian Panadero <panavtec@gmail.com>
2018-06-11 13:24:44 +0100

    use new functions

--- config/emacs/.emacs.d/lisp/init-clipboard.el
+++ config/emacs/.emacs.d/lisp/init-clipboard.el
@@ -3,8 +3,8 @@
   (if (region-active-p)
     (progn
       (cond
-        ((and (display-graphic-p) x-select-enable-clipboard)
-         (x-set-selection 'CLIPBOARD (buffer-substring (region-beginning) (region-end))))
+        ((and (display-graphic-p) select-enable-clipboard)
+         (gui-set-selection 'CLIPBOARD (buffer-substring (region-beginning) (region-end))))
         (t (shell-command-on-region (region-beginning) (region-end)
                                     (cond
                                       (*cygwin* "putclip")
