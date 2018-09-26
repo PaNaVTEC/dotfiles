28ff5a58d1e7b74851f06f9d8fc657af46bf066d
Christian Panadero <panavtec@gmail.com>
2018-09-26 22:31:30 +0100

    add good spelling mode to activate spelling when needed

--- config/emacs/.emacs.d/lisp/init-text.el
+++ config/emacs/.emacs.d/lisp/init-text.el
@@ -1,15 +1,24 @@
-(add-hook 'text-mode-hook
-          (lambda () (progn
-                  (wc-mode +1)
-                  (writegood-mode +1)
-                  (flyspell-mode +1)
-                  (flyspell-buffer))))
-
 (use-package helm-flyspell :ensure t :defer t)
 (use-package wc-mode :diminish flyspell-mode :ensure t :defer t)
 (use-package writegood-mode :diminish writegood-mode :ensure t :defer t)
 (use-package writeroom-mode :diminish writeroom-mode :ensure t :defer t)
 
-(evil-leader/set-key "c" 'helm-flyspell-correct)
+(define-minor-mode
+  good-spelling-mode
+  :ligter " T"
+  :group 'text
+
+  (evil-leader/set-key "c" 'helm-flyspell-correct)
+
+  (if good-spelling-mode
+    (progn
+      (wc-mode +1)
+      (writegood-mode +1)
+      (flyspell-mode +1)
+      (flyspell-buffer))
+    (progn
+      (wc-mode -1)
+      (writegood-mode -1)
+      (flyspell-mode -1))))
 
 (provide 'init-text)
