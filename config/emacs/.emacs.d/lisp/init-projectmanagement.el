4b32034bc3d152cefae51b34ffcde82b58bd6882
Christian Panadero <panavtec@gmail.com>
2019-01-22 19:21:34 +0000

    assign perspective shortcuts

--- config/emacs/.emacs.d/lisp/init-projectmanagement.el
+++ config/emacs/.emacs.d/lisp/init-projectmanagement.el
@@ -10,6 +10,10 @@
   :after projectile
   :config
   (persp-mode)
+
+  (define-key evil-normal-state-map (kbd "[p") 'persp-prev)
+  (define-key evil-normal-state-map (kbd "]p") 'persp-next)
+  (evil-leader/set-key "p" 'projectile-persp-switch-project)
   (use-package persp-projectile :ensure t))
 
 ;; CtrlP like
