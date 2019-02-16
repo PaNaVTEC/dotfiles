87ab98fc8d80fd74b97ba3141ab3e851e648b827
Carlos Morera de la Chica <carlosdelachica@gmail.com>
2019-02-16 12:36:40 +0000

    Add emacs indentation mode

---
+++ config/emacs/.emacs.d/lisp/init-indentation.el
@@ -0,0 +1,11 @@
+(require 'paren)
+
+(use-package
+  highlight-indent-guides
+  :ensure t
+  :defer t
+  :config
+  (setq highlight-indent-guides-method 'character)
+  (setq highlight-indent-guides-responsive 'top))
+
+(provide 'init-indentation)
