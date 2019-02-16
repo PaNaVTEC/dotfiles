3d41fab916ebebb7d9e95301cd52a106f6c2859c
Christian Panadero <panavtec@gmail.com>
2019-02-16 08:07:11 +0000

    refactor to stack

--- setup/src/Commands.hs
+++ setup/src/Commands.hs
@@ -5,10 +5,12 @@
 
 module Commands where
 
-import           Turtle
-import           Data.Text           (pack, strip, unpack)
-import           Data.List
 import qualified Data.ByteString.Char8 as B
+import           Data.List
+import           Data.Text                   (pack)
+import           Turtle
+
+type ExecResult = Either Text Text
 
 run :: String -> Shell Line
 run command = inshell (pack command) empty
@@ -16,39 +18,46 @@ run command = inshell (pack command) empty
 run' :: MonadIO io => String -> io ExitCode
 run' command = shell (pack command) empty
 
-runpenv :: MonadIO io => [String] -> io ExitCode
-runpenv = run' . join . intersperse " && "
+prun :: MonadIO io => Text -> io ExecResult
+prun command = toOutput <$> shellStrictWithErr command empty
+  where
+    toOutput (ExitSuccess, out, _) = Right out
+    toOutput (_, _, e') = Left e'
 
-lnsfn :: MonadIO io => Turtle.FilePath -> Turtle.FilePath -> io ExitCode
-lnsfn s' t = run' $ "ln -sfn " ++ encodeString s' ++ " " ++ encodeString t
+runpenv :: MonadIO io => [String] -> io ExecResult
+runpenv = prun . pack . join . Data.List.intersperse " && "
 
-sudolnsfn :: MonadIO io => Turtle.FilePath -> Turtle.FilePath -> io ExitCode
-sudolnsfn s' t = run' $ "sudo ln -sfn " ++ encodeString s' ++ " " ++ encodeString t
+lnsfn :: MonadIO io => Turtle.FilePath -> Turtle.FilePath -> io ExecResult
+lnsfn s' t = prun . pack $ "ln -sfn " ++ encodeString s' ++ " " ++ encodeString t
 
-pacmanSync :: MonadIO io => io ExitCode
-pacmanSync = run' "sudo pacman -Syy"
+sudolnsfn :: MonadIO io => Turtle.FilePath -> Turtle.FilePath -> io ExecResult
+sudolnsfn s' t = prun .pack $ "sudo ln -sfn " ++ encodeString s' ++ " " ++ encodeString t
 
-pacmanUpdate :: MonadIO io => io ExitCode
-pacmanUpdate = run' "sudo pacman -Syu --noconfirm"
+pacmanSync :: MonadIO io => io ExecResult
+pacmanSync = prun "sudo pacman -Syy"
+
+pacmanUpdate :: MonadIO io => io ExecResult
+pacmanUpdate = prun "sudo pacman -Syu --noconfirm"
 
 class ToPkgName a where toPkgName :: a -> PkgName
 instance ToPkgName String where toPkgName = PkgName
 
 newtype PkgName = PkgName { unPkgName :: String } deriving (Eq, Show, IsString)
 
-pacmanInstall :: MonadIO io => PkgName -> io ExitCode
-pacmanInstall pkg = run' $ "sudo pacman -S --noconfirm --needed " ++ unPkgName pkg
+pacmanInstall :: MonadIO io => PkgName -> io ExecResult
+pacmanInstall pkg = prun . pack $ "sudo pacman -S --noconfirm --needed " <> unPkgName pkg
 
-pacmanInstall' :: MonadIO io => [PkgName] -> io ExitCode
-pacmanInstall' pkgs = run' $ "sudo pacman -S --noconfirm --needed " ++ (join . intersperse " " $ unPkgName <$> pkgs)
+pacmanInstall' :: MonadIO io => [PkgName] -> io ExecResult
+pacmanInstall' pkgs = prun . pack
+  $ "sudo pacman -S --noconfirm --needed " <> (join . Data.List.intersperse " " $ unPkgName <$> pkgs)
 
-aurInstall :: MonadIO io => PkgName -> io ExitCode
-aurInstall pkg = run' $ "yay --noconfirm -S --needed " ++ unPkgName pkg
+aurInstall :: MonadIO io => PkgName -> io ExecResult
+aurInstall pkg = prun . pack $ "yay --noconfirm -S --needed " ++ unPkgName pkg
 
-aurInstall' :: MonadIO io => [PkgName] -> io ExitCode
-aurInstall' pkgs = run' $ "yay --noconfirm -S --needed " ++ (join . intersperse " " $ unPkgName <$> pkgs)
+aurInstall' :: MonadIO io => [PkgName] -> io ExecResult
+aurInstall' pkgs = prun . pack $ "yay --noconfirm -S --needed " ++ (join . Data.List.intersperse " " $ unPkgName <$> pkgs)
 
-aurInstallF :: MonadIO io => Prelude.FilePath -> io ExitCode
+aurInstallF :: MonadIO io => Prelude.FilePath -> io ExecResult
 aurInstallF fp' = do
   pkgs <- liftIO $ B.lines <$> B.readFile fp'
   head $ aurInstall . PkgName . B.unpack <$> pkgs
@@ -73,43 +82,52 @@ line &> filepath = do
   touch filepath
   output filepath line
 
-yarnInstallG :: MonadIO io => PkgName -> io ExitCode
-yarnInstallG pkg = run' $ "yarn global add " ++ unPkgName pkg
+yarnInstallG :: MonadIO io => PkgName -> io ExecResult
+yarnInstallG pkg = prun . pack $ "yarn global add " <> unPkgName pkg
 
-yarnInstallG' :: MonadIO io => [PkgName] -> io ExitCode
-yarnInstallG' pkgs = run' $ "yarn global add " ++ (join . intersperse " " $ unPkgName <$> pkgs)
+yarnInstallG' :: MonadIO io => [PkgName] -> io ExecResult
+yarnInstallG' pkgs = prun . pack $ "yarn global add " <> (join . Data.List.intersperse " " $ unPkgName <$> pkgs)
 
-stackInstall :: MonadIO io => [PkgName] -> io ExitCode
-stackInstall pkgs = run' $ "stack install " ++ (join . intersperse " " $ unPkgName <$> pkgs)
+stackInstall :: MonadIO io => [PkgName] -> io ExecResult
+stackInstall pkgs = prun . pack $ "stack install " <> (join . Data.List.intersperse " " $ unPkgName <$> pkgs)
 
-sudorm :: MonadIO io => Turtle.FilePath -> io ExitCode
-sudorm s' = run' $ "sudo rm " ++ encodeString s'
+sudorm :: MonadIO io => Turtle.FilePath -> io ExecResult
+sudorm s' = prun . pack $ "sudo rm " ++ encodeString s'
 
-sudormdir :: MonadIO io => Turtle.FilePath -> io ExitCode
-sudormdir s' = run' $ "sudo rm -rf " ++ encodeString s'
+sudormdir :: MonadIO io => Turtle.FilePath -> io ExecResult
+sudormdir s' = prun . pack $ "sudo rm -rf " ++ encodeString s'
 
-sudomv :: MonadIO io => Turtle.FilePath -> Turtle.FilePath -> io ExitCode
-sudomv s' t = run' $ "sudo mv " ++ encodeString s' ++ " " ++ encodeString t
+sudomv :: MonadIO io => Turtle.FilePath -> Turtle.FilePath -> io ExecResult
+sudomv s' t = prun . pack $ "sudo mv " ++ encodeString s' ++ " " ++ encodeString t
 
-startService :: MonadIO io => String -> io ()
+startService :: MonadIO io => Text -> io ()
 startService serv = do
   isEnabled <- exitsOk $ "systemctl is-enabled " <> serv
   unless isEnabled $
     do
-    _ <- run' $ "sudo systemctl enable " <> serv
-    _ <- run' $ "sudo systemctl start " <> serv
+    _ <- prun $ "sudo systemctl enable " <> serv
+    _ <- prun $ "sudo systemctl start " <> serv
     return ()
 
-addUserToGroup :: (MonadIO io, Show u, Show g) => u -> g -> io ExitCode
-addUserToGroup user group' =
-  run' $ "sudo usermod -a -G " <> show group' <> " " <> show user
+addCurrentUserToGroup :: (MonadIO io) => Text -> io ExecResult
+addCurrentUserToGroup group' = do
+  res' <- currentUser
+  case res' of
+    (Right u') -> addUserToGroup u' group'
+    t -> pure t
 
-commandExists :: MonadIO io => String -> io Bool
+addUserToGroup :: (MonadIO io) => Text -> Text -> io ExecResult
+addUserToGroup user group' =
+  prun $ "sudo usermod -a -G " <> group' <> " " <> user
+
+commandExists :: MonadIO io => Text -> io Bool
 commandExists cmd' = exitsOk $ "which " <> cmd'
 
-exitsOk :: MonadIO io => String -> io Bool
-exitsOk cmd' = (== ExitSuccess) <$> run' cmd'
+exitsOk :: MonadIO io => Text -> io Bool
+exitsOk cmd' = exitsOk' <$> prun cmd'
+  where
+    exitsOk' (Right _) = True
+    exitsOk' _ = False
 
-currentUser :: MonadIO io => io Text
-currentUser = parseUser <$> strict (inshell (pack "id -u -n") empty)
-  where parseUser = read . unpack . strip
+currentUser :: MonadIO io => io ExecResult
+currentUser = prun "id -u -n"
--- setup/src/Lib.hs
+++ setup/src/Lib.hs
@@ -1,17 +1,32 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedStrings          #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE FlexibleContexts #-}
 
 module Lib (entryPoint) where
 
-import           Turtle
 import           Commands
-import           Network.Wreq
-import qualified Data.ByteString.Char8 as B
-import qualified Data.ByteString.Lazy as BL
-import           Data.Maybe (catMaybes)
 import           Control.Lens
+import           Control.Monad.Except
+import           Control.Monad.Trans
+import qualified Data.ByteString.Char8      as B
+import qualified Data.ByteString.Lazy       as BL
+import           Data.Maybe                        (catMaybes)
+import           Network.Wreq
+import           Turtle
+
+type App m = AppT m ()
+newtype AppT m a = App
+  { unApp :: ExceptT Text m a
+  } deriving
+  (Functor, Applicative, Monad, MonadIO, MonadError Text)
 
 entryPoint :: IO ()
-entryPoint = do
+entryPoint = void . runExceptT . unApp $ install'
+
+install' :: MonadIO io => App io
+install' = do
   configurePacman
   installPacmanWrapper
   _ <- aurInstallF "./yaourt_i3.txt"
@@ -26,24 +41,23 @@ entryPoint = do
   _ <- aurInstallF "./yaourt_audio.txt"
   return ()
 
-installPrinter :: IO ()
+installPrinter :: MonadIO io => App io
 installPrinter = do
   _ <- aurInstall' ["cups", "nss-mdns", "gtk3-print-backends"]
   startService "orgs.cups.cupsd.service"
   startService "avahi-daemon.service"
-  putStrLn "Add your printer in: http://localhost:631/"
-  u' <- currentUser
-  _ <- addUserToGroup u' ("sys" :: Text)
+  liftIO $ putStrLn "Add your printer in: http://localhost:631/"
+  _ <- addCurrentUserToGroup "sys"
   return ()
 
-installBt :: IO ()
+installBt :: MonadIO io => App io
 installBt = do
   _ <- aurInstall' ["bluez", "bluez-utils", "bluez-qt", "pulseaudio-bluetooth"]
   _ <- run' "modprobe btusb"
   startService "bluetooth.service"
   return ()
 
-installDevTools :: IO ()
+installDevTools :: MonadIO io => App io
 installDevTools = do
   installGit
   _ <- aurInstallF "./yaourt_java.txt"
@@ -60,14 +74,14 @@ installDevTools = do
   installDocker
   installIntellij
 
-installIntellij :: IO ()
+installIntellij :: MonadIO io => App io
 installIntellij = do
-  B.writeFile "/tmp/sysctl.d/99-sysctl.conf" "fs.inotify.max_user_watches = 524288"
+  liftIO $ B.writeFile "/tmp/sysctl.d/99-sysctl.conf" "fs.inotify.max_user_watches = 524288"
   _ <- sudomv "/tmp/sysctl.d/99-sysctl.conf""/etc/sysctl.d/99-sysctl.conf"
   _ <- run' "sudo sysctl --system"
   return ()
 
-installDocker :: IO ()
+installDocker :: MonadIO io => App io
 installDocker = do
   _ <- sudormdir "/var/lib/docker"
   dockerlib <- (~/) ".dockerlib"
@@ -75,7 +89,7 @@ installDocker = do
   _ <- sudolnsfn dockerlib "/var/lib/docker"
   return ()
 
-installRust :: IO ()
+installRust :: MonadIO io => App io
 installRust = do
   _ <- aurInstall "rustup"
   _ <- run' "rustup toolchain install stable"
@@ -83,7 +97,7 @@ installRust = do
   _ <- run' "rustup toolchain install nightly"
   return ()
 
-installJs :: IO ()
+installJs :: MonadIO io => App io
 installJs = do
   _ <- aurInstall' ["nodejs", "npm", "yarn"]
   _ <- run' "yarn config set -- --emoji true"
@@ -92,23 +106,23 @@ installJs = do
   _ <- yarnInstallG' ["tern", "standard", "create-react-app", "js-beautify", "typescript", "tslint", "eslint-plugin-typescript", "typescript-eslint-parser"]
   return ()
 
-installHaskell :: IO ()
+installHaskell :: MonadIO io => App io
 installHaskell = do
   _ <- aurInstall "stack-bin"
   _ <- run' "stack setup"
   _ <- stackInstall ["ghcid", "hindent", "stylish-haskell", "cabal-install", "hoogle", "hlint"]
   return ()
 
-installGo :: IO ()
+installGo :: MonadIO io => App io
 installGo = do
   _    <- aurInstall "go"
   bin  <- (~/) "go/bin"
   src  <- (~/) "go/src"
-  mktrees [bin, src]
+  liftIO $ mktrees [bin, src]
   cmdE <- commandExists "golint"
   unless cmdE $ void $ run' "go get -u github.com/golang/lint/golint"
 
-installGit :: IO ()
+installGit :: MonadIO io => App io
 installGit = sh $ do
   _ <- aurInstallF "./yaourt_git.txt"
   createGitIgnore
@@ -121,18 +135,18 @@ installGit = sh $ do
       append path ".tern-project"
       append path ".tern-port"
 
-installCompton :: IO ()
+installCompton :: MonadIO io => App io
 installCompton = do
   _ <- aurInstall' ["compton", "xorg-xwininfo"]
   beforeStartX <- (~/) ".before_startx"
   mktree beforeStartX
   let runsh = beforeStartX </> "run.sh"
   touch runsh
-  B.writeFile (encodeString runsh) "compton -c -i 0.9 -b &"
+  liftIO $ B.writeFile (encodeString runsh) "compton -c -i 0.9 -b &"
   _ <- chmodx runsh
   return ()
 
-configurePacman :: IO ()
+configurePacman :: MonadIO io => App io
 configurePacman = do
   _ <- sudorm "/etc/pacman.conf"
   _ <- (`sudolnsfn` "/etc/pacman.conf") =<< pwd' "config/pacman/pacman.conf"
@@ -143,7 +157,7 @@ configurePacman = do
   _ <- pacmanUpdate
   return ()
 
-installPacmanWrapper :: IO ()
+installPacmanWrapper :: MonadIO io => App io
 installPacmanWrapper = do
   _ <- pacmanInstall' ["base-devel", "git", "wget", "yajl", "go"]
   _ <- installYay
@@ -158,10 +172,10 @@ installPacmanWrapper = do
         ,"makepkg -si --noconfirm"
         ]
 
-updateMirrorList :: IO ()
+updateMirrorList :: MonadIO io => App io
 updateMirrorList = do
-  r <- get "https://www.archlinux.org/mirrorlist/?country=GB&protocol=https"
-  B.writeFile "/tmp/mirrorlist" (uncommentLines $ r ^. responseBody)
+  r <- liftIO $ get "https://www.archlinux.org/mirrorlist/?country=GB&protocol=https"
+  liftIO $ B.writeFile "/tmp/mirrorlist" (uncommentLines $ r ^. responseBody)
   _ <- sudorm "/etc/pacman.d/mirrorlist"
   _ <- sudomv "/tmp/mirrorlist" "/etc/pacman.d/mirrorlist"
   return ()
