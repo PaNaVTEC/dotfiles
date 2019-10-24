e3b56fee2359d9a95ea473682f68eafc0d89257d
Christian Panadero <panavtec@gmail.com>
2019-10-24 18:25:31 +0100

    align rule for haskell imports

--- config/emacs/.emacs.d/lisp/init-haskell.el
+++ config/emacs/.emacs.d/lisp/init-haskell.el
@@ -61,6 +61,10 @@
                  (regexp . "\\(\\s-+\\)\\(::\\|∷\\)\\s-+")
                  (modes quote (haskell-mode literate-haskell-mode))))
   (add-to-list 'align-rules-list
+               '(haskell-types
+                 (regexp . "\\(\\s-+\\)\\as\\s-+")
+                 (modes quote (haskell-mode literate-haskell-mode))))
+  (add-to-list 'align-rules-list
                '(haskell-assignment
                  (regexp . "\\(\\s-+\\)=\\s-+")
                  (modes quote (haskell-mode literate-haskell-mode))))
