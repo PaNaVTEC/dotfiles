bfd8182e1ff5fda019ab9ac8afd2ea31ba12781e
Christian Panadero <panavtec@gmail.com>
2019-11-26 12:39:21 +0000

    add copy file path to clipboard func

--- config/emacs/.emacs.d/lisp/init-file-explorer.el
+++ config/emacs/.emacs.d/lisp/init-file-explorer.el
@@ -37,4 +37,35 @@
 (location-list-buffer (rx bos "*helm-mode-neotree-"))
 (location-list-buffer (rx bos "*helm-mode-neo-buffer--rename-node*"))
 
+(defun copy-file-path (&optional @dir-path-only-p)
+  "Copy the current buffer's file path or dired path to `kill-ring'.
+Result is full path.
+If `universal-argument' is called first, copy only the dir path.
+
+If in dired, copy the file/dir cursor is on, or marked files.
+
+If a buffer is not file and not dired, copy value of `default-directory' (which is usually the “current” dir when that buffer was created)
+
+URL `http://ergoemacs.org/emacs/emacs_copy_file_path.html'
+Version 2017-09-01"
+  (interactive "P")
+  (let (($fpath
+         (if (string-equal major-mode 'dired-mode)
+             (progn
+               (let (($result (mapconcat 'identity (dired-get-marked-files) "\n")))
+                 (if (equal (length $result) 0)
+                     (progn default-directory )
+                   (progn $result))))
+           (if (buffer-file-name)
+               (buffer-file-name)
+             (expand-file-name default-directory)))))
+    (kill-new
+     (if @dir-path-only-p
+         (progn
+           (message "Directory path copied: 「%s」" (file-name-directory $fpath))
+           (file-name-directory $fpath))
+       (progn
+         (message "File path copied: 「%s」" $fpath)
+         $fpath )))))
+
 (provide 'init-file-explorer)
