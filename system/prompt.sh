441d8d93302b91e7625ef8f9b8b2d4a3f1d52b42
Christian Panadero <panavtec@gmail.com>
2020-04-11 11:55:24 +0200

    add nix support to powerline

--- system/prompt.sh
+++ system/prompt.sh
@@ -3,8 +3,17 @@
 export PROMPT_DIRTRIM=2
 
 function _update_ps1() {
-  PS1="$(powerline-go -cwd-mode plain -modules venv,ssh,cwd,perms,git,docker,jobs,exit,root -error $?)"
+  PS1="$(powerline-go -cwd-mode plain -modules venv,ssh,cwd,perms,git,nix-shell,docker,jobs,exit,root -error $?)"
 }
+
+# Workaround for nix-shell --pure
+if [ "$IN_NIX_SHELL" == "pure" ]; then
+  if [ -x "$HOME/.nix-profile/bin/powerline-go" ]; then
+    alias powerline-go="$HOME/.nix-profile/bin/powerline-go"
+  elif [ -x "/run/current-system/sw/bin/powerline-go" ]; then
+    alias powerline-go="/run/current-system/sw/bin/powerline-go"
+  fi
+fi
 
 if [ "$TERM" != "linux" ]; then
   PROMPT_COMMAND="history -a; $PROMPT_COMMAND"
