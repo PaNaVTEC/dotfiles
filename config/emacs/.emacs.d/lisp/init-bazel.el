c145a6dd614b5102bf879dab7e8e0c4cb86cdfae
Christian Panadero <panavtec@gmail.com>
2020-10-09 11:07:30 +0200

    use elpa bazel mode

--- config/emacs/.emacs.d/lisp/init-bazel.el
+++ config/emacs/.emacs.d/lisp/init-bazel.el
@@ -1,9 +1,12 @@
-(require 'bazel-mode)
+(require 'init-programming-mode)
 
-(add-to-list 'load-path "/path/to/bazel-mode/directory")
-(autoload 'bazel-mode "bazel-mode" "Major mode for Bazel BUILD files." t)
-(add-to-list 'auto-mode-alist '("/BUILD\\(\\..*\\)?\\'" . bazel-mode))
-(add-to-list 'auto-mode-alist '("/WORKSPACE\\'" . bazel-mode))
-(add-to-list 'auto-mode-alist '("\\.\\(BUILD\\|WORKSPACE\\|bzl\\)\\'" . bazel-mode))
+(use-package
+  bazel-mode
+  :ensure t
+  :defer t
+  :mode "\\.ba?ze?l$"
+  :config
+  (add-hook 'bazel-mode-hook 'programming-mode)
+)
 
 (provide 'init-bazel)
