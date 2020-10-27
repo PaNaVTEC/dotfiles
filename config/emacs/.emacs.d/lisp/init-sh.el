23ae602c749607863d4d31ad8233612166c9ffaa
Christian Panadero <panavtec@gmail.com>
2020-10-27 09:55:01 +0100

    enables shellcheck

--- config/emacs/.emacs.d/lisp/init-sh.el
+++ config/emacs/.emacs.d/lisp/init-sh.el
@@ -2,6 +2,15 @@
 
 (add-hook 'sh-mode-hook 'programming-mode)
 (add-hook 'sh-mode-hook 'sh-doc)
+
+(add-hook
+  'sh-mode-hook
+  (lambda ()
+    (progn
+      (programming-mode)
+      (sh-doc)
+      (add-hook 'flycheck-mode-hook (lambda () (progn (flycheck-add-mode 'sh-shellcheck 'sh-mode)))))))
+
 (setq sh-basic-offset 2 sh-indentation 2)
 
 (defun sh-doc ()
