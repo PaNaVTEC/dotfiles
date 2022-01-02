f586850a952f465362a88af2a426131830c0993b
Christian Panadero <panavtec@gmail.com>
2022-01-02 19:48:12 +0100

    blueman config fix

--- nixos/bluetooth.nix
+++ nixos/bluetooth.nix
@@ -2,18 +2,5 @@
 
 {
   hardware.bluetooth.enable = true;
-
-  environment.systemPackages = with pkgs; [
-    blueman
-  ];
-
-  hardware.pulseaudio = {
-    # Only the full build has Bluetooth support, so it must be selected here.
-    package = pkgs.pulseaudioFull;
-  };
-
-  boot.extraModprobeConfig = ''
-    options snd-hda-intel model=dell-headset-multi
-  '';
-
+  services.blueman.enable = true;
 }
