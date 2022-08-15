5365d6505998bf1d19f80bc9b7c1d3be0c1ab5cf
Christian Panadero <panavtec@gmail.com>
2022-08-15 11:30:16 +0200

    got rid of old setup based on arch

--- laptops/NUCi7/setup.sh
+++
@@ -1,1 +0,0 @@
-bash -c 'echo -e "options snd-hda-intel model=dell-headset-multi" > /etc/modprobe.d/alsa-base.conf'
--- setup/app/Main.hs
+++
@@ -1,6 +0,0 @@
-module Main where
-
-import Lib (entryPoint)
-
-main :: IO ()
-main = entryPoint
